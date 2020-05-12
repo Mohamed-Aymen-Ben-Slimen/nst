b9fe43b90b0d9e4fc0088e623a1a5f540a2f027c
Mohamed-Aymen-Ben-Slimen <50459819+Mohamed-Aymen-Ben-Slimen@users.noreply.github.com>
2020-05-12 23:55:33 +0100

    v_1 docs

---
+++ docs/precache-manifest.32c51a5ced30f4cd671dff200d7941cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c75789b3c6f04770d6e683eef8070c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "593987705e40c7812960",
+    "url": "/static/css/main.d859257c.chunk.css"
+  },
+  {
+    "revision": "900d3eafd3e810fe10fc",
+    "url": "/static/js/2.57c0ff0d.chunk.js"
+  },
+  {
+    "revision": "6df4548ecb01cd88e57d1234281566da",
+    "url": "/static/js/2.57c0ff0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593987705e40c7812960",
+    "url": "/static/js/main.5ab96ee3.chunk.js"
+  },
+  {
+    "revision": "9efb82a55350d84b93bf",
+    "url": "/static/js/runtime-main.7c502e96.js"
+  }
+]);
